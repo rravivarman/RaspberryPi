a1d1fc93a5e4950b23f3f27082ffbdcd163cecf5
RAVIVARMAN RAJENDIRAN <dreamerindia@gmail.com>
2020-10-09 16:01:21 +0530

    ADS1115 LDR
    ADS1115 LDR

---
+++ ADS1115 LDR/ldr_read.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#encoding:utf-8
+
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1015(i2c)
+ads.gain = 1
+
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P0)
+
+print("{:>5}\t{:>5}".format('raw', 'v'))
+
+while True:
+    print("{:>5}\t{:>5.5f}".format(chan.value, chan.voltage))
+    time.sleep(0.5)
